a98d19adf4b8882b50674bd50ebf612c36dee183
MohamedRazak <mohamed.razak@adp.com>
2021-02-18 00:07:22 +0530

    https://leetcode.com/problems/reverse-integer/

---
+++ reverseInteger.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+const reverse = function(x) {
+    let reversedNumberArr = [];
+    let absoluteValOfX = Math.abs(x);
+    const maxValueAllowed = 0x7fffffff;
+    while(absoluteValOfX){
+        reversedNumberArr.push(absoluteValOfX % 10);
+        absoluteValOfX = Math.floor(absoluteValOfX/10);
+    }
+    let reversedNumber = x >= 0 ? +Number(reversedNumberArr.join("")) : -Number(reversedNumberArr.join(""));
+    if (Math.abs(reversedNumber) > maxValueAllowed) {
+        return 0;
+    }
+    return reversedNumber;
+};
+
+console.log(reverse(-2147483648));
