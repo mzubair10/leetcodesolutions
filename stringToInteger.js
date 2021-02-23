d56101bd3c4756b38e8492f37580d053585126b2
MohamedRazak <mohamed.razak@adp.com>
2021-02-23 14:20:32 +0530

    final code

--- stringToInteger.js
+++ stringToInteger.js
@@ -1,41 +1,34 @@
 // jshint esversion:6
 const myAtoi = function (s) {
-  let trimmedInput = s.trim();
-  const maxPositiveNumber = 0x7fffffff;
-  const minNegativeNumber = -0x80000000;
-  let signAssociated = "+";
-  
-  if (trimmedInput.length > 0 && (trimmedInput[0].includes("+") || (trimmedInput[0].includes("-")))){
-    if (trimmedInput[0] == "-"){
-        signAssociated = "-";
+    let trimmedInput = s.trim();
+    const maxPositiveNumber = 0x7fffffff;
+    const minNegativeNumber = -0x80000000;
+    let signAssociated = "+";
+    
+    if (trimmedInput.length > 0 && (trimmedInput[0].includes("+") || (trimmedInput[0].includes("-")))){
+      if (trimmedInput[0] == "-"){
+          signAssociated = "-";
+      }
+      trimmedInput = trimmedInput.split("").slice(1);
     }
-    trimmedInput = trimmedInput.split("").slice(1);
-  }
-//   if (s < 0) {
-//     signAssociated = "-";
-//     //trimmedInput = trimmedInput.split("").slice(1);
-//   }
-  //const parsedInput = trimmedInput.split("");
-  let parsedNumericInput = [];
-  for (let iter = 0; iter < trimmedInput.length; iter++) {
-    if (trimmedInput[iter] >= "0" && trimmedInput[iter] <= "9") {
-      parsedNumericInput.push(trimmedInput[iter]);
-    } else {
-      break;
+    let parsedNumericInput = [];
+    for (let iter = 0; iter < trimmedInput.length; iter++) {
+      if (trimmedInput[iter] >= "0" && trimmedInput[iter] <= "9") {
+        parsedNumericInput.push(trimmedInput[iter]);
+      } else {
+        break;
+      }
     }
-  }
-  let finalParsedNumericInput = parsedNumericInput.join("");
-  if (
-    finalParsedNumericInput.toString(16) > maxPositiveNumber ||
-    finalParsedNumericInput.toString(16) < minNegativeNumber
-  ) {
-    if (signAssociated == "+") {
-      return maxPositiveNumber;
-    } else {
-      return minNegativeNumber;
+    let finalParsedNumericInput = parsedNumericInput.join("");
+    if (
+      finalParsedNumericInput.toString(16) > maxPositiveNumber ||
+      finalParsedNumericInput.toString(16) < minNegativeNumber
+    ) {
+      if (signAssociated == "+") {
+        return maxPositiveNumber;
+      } else {
+        return minNegativeNumber;
+      }
     }
-  }
-  return Number(signAssociated.concat(Math.abs(finalParsedNumericInput)));
-};
-
-console.log(myAtoi("  -0012a42"));
+    return Number(signAssociated.concat(Math.abs(finalParsedNumericInput)));
+  };
